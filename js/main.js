62f18f3e19a2efc67eb5fbc25eeac1b49de60ba8
Reubi0910 <esteban.morillo@divtec.ch>
2023-09-26 15:06:29 +0200

    ajout code js

--- js/main.js
+++ js/main.js
@@ -5,4 +5,33 @@
 
 'use strict';
 
-console.log('Test liaison JS');
+// récupération du <body>
+const body = document.querySelector('body');
+
+const listeBg = document.querySelector('#bg');
+console.log(listeBg);
+body.style.backgroundImage = `url(./img/backgrounds/${listeBg.value}.jpg)`;
+listeBg.addEventListener('change', () => {
+    body.style.backgroundImage = `url(./img/backgrounds/${listeBg.value}.jpg)`;
+});
+
+/******************
+ ENVOI DU FORMULAIRE
+ ******************/
+// récupération du formulaire et des champs mot de passe
+const form = document.querySelector('form');
+const pwd = document.querySelector('#pwd');
+const pwdconfirm = document.querySelector('#pwdconfirm');
+
+// Ecouter l'envoi du formulaire
+form.addEventListener('submit', (e) => {
+    // teste si les mots de passe sont identiques
+    if (pwd.value !== pwdconfirm.value) {
+        // Annuler l'envoi du formulaire
+        e.preventDefault();
+        // Afficher un message d'erreur
+        alert('Les mots de passe ne sont pas identiques');
+        return;
+    }
+
+});
