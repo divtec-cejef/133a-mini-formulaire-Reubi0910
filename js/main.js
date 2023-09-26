45518a343490388cf044ea23f9cb97c00a3d873b
Reubi0910 <esteban.morillo@divtec.ch>
2023-09-26 15:27:48 +0200

    ajout test envoi

--- js/main.js
+++ js/main.js
@@ -24,14 +24,25 @@ const pwd = document.querySelector('#pwd');
 const pwdconfirm = document.querySelector('#pwdconfirm');
 
 // Ecouter l'envoi du formulaire
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', (event) => {
+    // stopper l'envoi du formulaire
+    event.preventDefault();
+
+    // teste si le mots de passe à minimum 3 caractères
+    if (pwd.value.length < 3) {
+        // Afficher un message d'erreur
+        alert('Le mot de passe doit contenir au moins 3 caractères');
+        return;
+    }
+
     // teste si les mots de passe sont identiques
     if (pwd.value !== pwdconfirm.value) {
-        // Annuler l'envoi du formulaire
-        e.preventDefault();
         // Afficher un message d'erreur
         alert('Les mots de passe ne sont pas identiques');
         return;
     }
 
+    // envoi du formulaire
+    alert('compte créé avec succès');
+    form.submit();
 });
